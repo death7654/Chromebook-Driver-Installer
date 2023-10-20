52fe826b1fc42907e33bc25bc07fd793764e14a5
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2023-10-20 09:25:26 +0530

    added all files to install, and framework for installations 	renamed:    src-tauri/binaries/crosec.2.0.2-installer.exe -> src-tauri/binaries/crosec.2.0.2-installer-x86_64-pc-windows-msvc.exe 	renamed:    src-tauri/binaries/crostouchpad.4.1.4-onlyeve-installer.exe -> src-tauri/binaries/crostouchpad.4.1.4-onlyeve-installer-x86_64-pc-windows-msvc.exe 	renamed:    src-tauri/binaries/crostouchscreen.2.9.4-installer.exe -> src-tauri/binaries/crostouchscreen.2.9.4-installer-x86_64-pc-windows-msvc.exe 	renamed:    src-tauri/binaries/wilcoec.1.0.1-installer.exe -> src-tauri/binaries/wilcoec.1.0.1-installer-x86_64-pc-windows-msvc.exe 	modified:   src-tauri/src/main.rs 	modified:   src-tauri/tauri.conf.json 	modified:   src/main.js

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,7 @@ use std::os::windows::process::CommandExt;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_board_name])
+        .invoke_handler(tauri::generate_handler![get_board_name,check_process])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -29,3 +29,21 @@ async fn get_board_name() -> String {
         let boardname = boardnamelong.trim();
         return String::from(boardname);
     }
+#[tauri::command]
+async fn check_process() -> String {
+    let cmd: Result<std::process::Output, std::io::Error> =
+            std::process::Command::new("wmic")
+                    .creation_flags(0x08000000)
+                    .args(["process","where","\"name=\'crostouchscreen.2.9.4-installer-x86_64-pc-windows-msvc.exe\'\"","get","Captions"])
+                    .output();
+    
+            let output: String = match cmd {
+                Ok(output) => String::from_utf8_lossy(&output.stdout).split("\n").map(|x| x.to_string()).collect::<Vec<String>>()[0].clone(),
+                Err(e) => {
+                    println!("boardnameError `{}`.", e);
+                    String::from("deeznuts") // This match returns a blank string.
+                }
+            };
+            let output2 = output.trim();
+            return String::from(output2);
+        }
--- src/main.js
+++ src/main.js
@@ -2,14 +2,46 @@ import "./styles.css";
 import { invoke } from '@tauri-apps/api/tauri'
 import { Command } from "@tauri-apps/api/shell";
 
-const touchScreen = await Command.sidecar("./binaries/crostouchpad.4.1.4-installer", [
-  "/S",
-]);
-const boardname2 = await invoke("get_board_name");
+const ec = await Command.sidecar("./binaries/crosec.2.0.2-installer", ["/S",]);
+const touchPad = await Command.sidecar("./binaries/crostouchpad.4.1.4-installer", ["/S",]);
+const touchPadEve = await Command.sidecar("./binaries/crostouchpad.4.1.4-onlyeve-installer", ["/S",]);
+const touchScreen = await Command.sidecar("./binaries/crostouchscreen.2.9.4-installer", ["/S",]);
+const wilcoEc = await Command.sidecar("./binaries/wilcoec.1.0.1-installer", ["/S",]);
+const boardname = await invoke("get_board_name");
+
+
+setInterval(async () => {
+  let processExists = await invoke("check_process");
+  console.log(processExists)
+},1000)
 
 function installDrivers() {
+  console.log(boardname);
+/*
+  if (boardname !== "Eve") {
+    touchPad.execute();
+    console.log("installing touchpad");
+
+  }
+  else{
+    touchPadEve.execute();
+    console.log("installing eve touchpad");
+  }
+
+  if (boardname == "Wilco")
+  {
+    ec.execute();
+  }
+  else
+  {
+    wilcoEc.execute();
+    console.log("installing touchpad");
+  }
+*/
   touchScreen.execute();
-  console.log(boardname2);
+  console.log("installing touchscreen");
+
 
 }
+
 installDrivers();
