4f4ff9419440609f09e89b87e04206b2710c6804
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2023-10-20 21:05:20 +0530

    added process check + frontend 	modified:   README.md     modified:   index.html 	modified:   src-tauri/Cargo.lock 	modified:   src-tauri/Cargo.toml 	modified:   src-tauri/src/main.rs 	modified:   src-tauri/tauri.conf.json 	modified:   src/styles.css

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Tauri + Vue 3
-
-This template should help get you started developing with Tauri + Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
-
-## Recommended IDE Setup
-
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+<h4 class="dTitle">Disclaimer</h4>
+      <p>
+        Coolstar is the owner of all of the drivers included in this
+        application. I (death7654) simply made an easier way to install these
+        essential drivers and do not have any part in the development of these
+        drivers
+      </p>
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 
-use std::os::windows::process::CommandExt;
-
+use std::{os::windows::process::CommandExt};
+use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![get_board_name,check_process])
@@ -29,21 +29,12 @@ async fn get_board_name() -> String {
         let boardname = boardnamelong.trim();
         return String::from(boardname);
     }
-#[tauri::command]
-async fn check_process() -> String {
-    let cmd: Result<std::process::Output, std::io::Error> =
-            std::process::Command::new("wmic")
-                    .creation_flags(0x08000000)
-                    .args(["process","where","\"name=\'crostouchscreen.2.9.4-installer-x86_64-pc-windows-msvc.exe\'\"","get","Captions"])
-                    .output();
+    #[tauri::command]
+    async fn check_process() -> bool {
+        let system = System::new_all();
     
-            let output: String = match cmd {
-                Ok(output) => String::from_utf8_lossy(&output.stdout).split("\n").map(|x| x.to_string()).collect::<Vec<String>>()[0].clone(),
-                Err(e) => {
-                    println!("boardnameError `{}`.", e);
-                    String::from("deeznuts") // This match returns a blank string.
-                }
-            };
-            let output2 = output.trim();
-            return String::from(output2);
-        }
+        system.processes().into_iter().find(|(_, process)| 
+          process.name() == "crostouchscreen.2.9.4-installer-x86_64-pc-windows-msvc.exe"
+        ).is_some()
+    }
+        
