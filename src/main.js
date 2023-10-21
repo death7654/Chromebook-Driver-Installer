659e6786ca5be5b4b1f4e87f974ca3113bf16464
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2023-10-21 17:29:10 +0530

    Added realtek and i2s chipset and linked button 	modified:   index.html 	modified:   src-tauri/tauri.conf.json 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import { Command } from "@tauri-apps/api/shell";
 
 //sets up sidecars, ready to execute
 
-const crosVcDist = await Command.sidecar("./binaries/VC_redist.x64",["/quiet"]);
+const crosVcDist = await Command.sidecar("./binaries/VC_redist.x64", [
+  "/quiet",
+]);
 const crosEc = await Command.sidecar("./binaries/crosec.2.0.2-installer", [
   "/S",
 ]);
@@ -24,9 +26,21 @@ const crosWilcoEc = await Command.sidecar(
   "./binaries/wilcoec.1.0.1-installer",
   ["/S"]
 );
-const maxAudio = await Command.sidecar("./binaries/max98090.1.0.4-installer",["/S"]);
-const maxAudioR11 = await Command.sidecar('./binaries/max98090-r11.1.0.0-installer',["/S"])
-const audioSamus = await Command.sidecar('./binaries/csaudiosstcatpt.1.0.1-installer',["/S"]);
+const maxAudio = await Command.sidecar("./binaries/max98090.1.0.4-installer", [
+  "/S",
+]);
+const maxAudioR11 = await Command.sidecar(
+  "./binaries/max98090-r11.1.0.0-installer",
+  ["/S"]
+);
+const audioSamus = await Command.sidecar(
+  "./binaries/csaudiosstcatpt.1.0.1-installer",
+  ["/S"]
+);
+const i2s = await Command.sidecar("./binaries/BayTrailChipsetDriver-Lenovo", [
+  "/S",
+]);
+const realTek = await Command.sidecar("./binaries/alc5645audio", ["/S"]);
 
 //gets boardname
 const boardname = await invoke("get_board_name");
@@ -34,37 +48,67 @@ let hwid = await invoke("get_hwid");
 console.log(hwid);
 
 let processExists = null;
-let touchscreen = false;
-let max989090 = false;
 let samusAudio = false;
 
 //hardware ids identified in driver .infs
-const touchScreenHWID = ["ACPI\\ATML0001", "ACPI\\ELAN0001","ACPI\\MLFS0000","ACPI\\RAYD0001"]
-const max989090HWID = ["ACPI\\VEN_193C&DEV_9890&REV_0002","ACPI\\193C9890"]
+const touchScreenHWID = [
+  "ACPI\\ATML0001",
+  "ACPI\\ELAN0001",
+  "ACPI\\MLFS0000",
+  "ACPI\\RAYD0001",
+];
+const max989090HWID = ["ACPI\\VEN_193C&DEV_9890&REV_0002", "ACPI\\193C9890"];
+const i2sDriverBoardname = [
+  "Winky",
+  "Swanky",
+  "Squawks",
+  "Quawks",
+  "Orco",
+  "Kip",
+  "Heli",
+  "Gnawty",
+  "Glimmer",
+  "Engaurde",
+  "Clapper",
+  "Candy",
+  "Banjo",
+];
+const realTekDriver = [
+  "Banon",
+  "Edgar",
+  "Kefka",
+  "Reks",
+  "Relm",
+  "Setzer",
+  "Terra",
+  "Terra13",
+  "Ultima",
+  "Wizpig",
+  "Celes",
+];
 
 //checks if touchscreen is avaliable
-const touchScreenAvaliable = touchScreenHWID.some(element => {
+let touchscreen = touchScreenHWID.some((element) => {
   return hwid.includes(element);
 });
-if (touchScreenAvaliable == true)
-{
-  touchscreen = true;
-}
+
 //checks if there is a max989090 in both cyan and other chromebooks that have this
-const isMax989090 = max989090HWID.some(element => {
+let max989090 = max989090HWID.some((element) => {
   return hwid.includes(element);
 });
 
-if(isMax989090 == true)
-{
-  max989090 == true;
-}
+//checks if there is a need for the i2s driver
+let i2sDriver = i2sDriverBoardname.some((element) => {
+  return boardname.includes(element);
+});
+//checks if there is a need for the realtek driver
+let realTekAudio = realTekDriver.some((element) => {
+  return boardname.includes(element);
+});
 
-if(boardname == "Samus")
-{
-  samusAudio == true;
+if (boardname == "Samus") {
+  samusAudio = true;
 }
-
 
 //Assigns variables to file name to check if process has started
 const vcDist = "VC_redist.x64-x86_64-pc-windows-msvc.exe";
@@ -87,7 +131,20 @@ if (boardname == "Eve") {
 const touchScreenInstaller =
   "crostouchscreen.2.9.4-installer-x86_64-pc-windows-msvc.exe";
 
-const Audio = "audio";
+//assigns process name
+let Audio;
+if (max989090 == true) {
+  if (boardname == "Cyan") {
+    Audio = "max98090-r11.1.0.0-installer-x86_64-pc-windows-msvc.exe";
+  } else {
+    Audio = "max98090.1.0.4-installer-x86_64-pc-windows-msvc.exe";
+  }
+} else if (boardname == "Samus") {
+  Audio = "csaudiosstcatpt.1.0.1-installer-x86_64-pc-windows-msvc.exe";
+} else if (realTekAudio == true) {
+  Audio = "BayTrailChipsetDriver-Lenovo-x86_64-pc-windows-msvc.exe";
+}
+const i2sInstaller = "BayTrailChipsetDriver-Lenovo-x86_64-pc-windows-msvc.exe";
 
 let index = 0;
 let processName = [
@@ -95,6 +152,7 @@ let processName = [
   ECFile,
   touchPadInstaller,
   touchScreenInstaller,
+  i2sInstaller,
   Audio,
 ];
 let processStatus = 0;
@@ -104,16 +162,13 @@ let process;
 function installStatus() {
   if (processExists == null) {
     process = processName[0];
-
   } else if (processExists == false && processStatus == 0) {
     process = processName[index];
     installDrivers(process);
-
   } else if (processExists == true && processStatus == 0) {
     process = processName[index];
     //console.log(process);
     processStatus++;
-
   } else if (processExists == true && processStatus == 1) {
     if (processStatus < 2 || processStatus == 1) {
       processStatus++;
@@ -128,12 +183,11 @@ function installStatus() {
 
 //sets interval
 function startInstall() {
-setInterval(async () => {
-  installStatus();
-  processExists = await invoke("check_process", { value1: process });
-}, 1000);
+  setInterval(async () => {
+    installStatus();
+    processExists = await invoke("check_process", { value1: process });
+  }, 1000);
 }
-
 
 //executes sidecars in order
 async function installDrivers(driver) {
@@ -157,19 +211,25 @@ async function installDrivers(driver) {
     }
   } else if (touchscreen == true && driver == touchScreenInstaller) {
     crosTouchScreen.execute();
-  }else if (touchscreen == false && driver == touchScreenInstaller) {
+  } else if (touchscreen == false && driver == touchScreenInstaller) {
     index++;
-  }
-   else if (max989090 == true && driver == Audio) {
-    if(boardname == "Cyan")
-    {
+  } else if (i2sDriver == true && driver == i2sInstaller) {
+    i2s.execute();
+  } else if (i2sDriver == false && driver == i2sInstaller) {
+    index++;
+  } else if (max989090 == true && driver == Audio) {
+    if (boardname == "Cyan") {
       maxAudioR11.execute();
-    }
-    else{
+    } else {
       maxAudio.execute();
     }
-  }
-  else if (samusAudio == true && driver == Audio){
+  } else if (samusAudio == true && driver == Audio) {
     audioSamus.execute();
+  } else if (realTekAudio == true && driver == Audio) {
+    realTek.execute();
   }
 }
+
+document.getElementById("install").addEventListener('keydown',startInstall());
+document.getElementById("install").addEventListener('mousedown',startInstall());
+
