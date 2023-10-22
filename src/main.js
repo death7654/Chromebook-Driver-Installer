4ddb974c77e4e57904711f2e71e764009e647d51
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2023-10-22 13:24:53 +0530

    added close, finish screen, and install functions 	modified:   index.html 	modified:   src-tauri/Cargo.toml 	new file:   src-tauri/binaries/BayTrailChipsetDriver-Lenovo-x86_64-pc-windows-msvc.exe 	new file:   src-tauri/binaries/alc5645audio-x86_64-pc-windows-msvc.exe 	modified:   src-tauri/tauri.conf.json 	modified:   src/main.js 	modified:   src/styles.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { invoke } from "@tauri-apps/api/tauri";
 import { Command } from "@tauri-apps/api/shell";
+import { appWindow } from "@tauri-apps/api/window";
 
 //sets up sidecars, ready to execute
 
@@ -45,7 +46,7 @@ const realTek = await Command.sidecar("./binaries/alc5645audio", ["/S"]);
 //gets boardname
 const boardname = await invoke("get_board_name");
 let hwid = await invoke("get_hwid");
-console.log(hwid);
+//console.log(hwid);
 
 let processExists = null;
 let samusAudio = false;
@@ -106,7 +107,7 @@ let realTekAudio = realTekDriver.some((element) => {
   return boardname.includes(element);
 });
 
-if (boardname == "Samus") {
+if (boardname === "Samus") {
   samusAudio = true;
 }
 
@@ -114,14 +115,14 @@ if (boardname == "Samus") {
 const vcDist = "VC_redist.x64-x86_64-pc-windows-msvc.exe";
 
 let ECFile = null;
-if (boardname == "Wilco") {
+if (boardname === "Wilco") {
   ECFile = "wilcoec.1.0.1-installer-x86_64-pc-windows-msvc.exe";
 } else {
   ECFile = "crosec.2.0.2-installer-x86_64-pc-windows-msvc.exe";
 }
 
 let touchPadInstaller;
-if (boardname == "Eve") {
+if (boardname === "Eve") {
   touchPadInstaller =
     "crostouchpad.4.1.4-onlyeve-installer-x86_64-pc-windows-msvc.exe";
 } else {
@@ -133,15 +134,15 @@ const touchScreenInstaller =
 
 //assigns process name
 let Audio;
-if (max989090 == true) {
-  if (boardname == "Cyan") {
+if (max989090 === true) {
+  if (boardname === "Cyan") {
     Audio = "max98090-r11.1.0.0-installer-x86_64-pc-windows-msvc.exe";
   } else {
     Audio = "max98090.1.0.4-installer-x86_64-pc-windows-msvc.exe";
   }
-} else if (boardname == "Samus") {
+} else if (boardname === "Samus") {
   Audio = "csaudiosstcatpt.1.0.1-installer-x86_64-pc-windows-msvc.exe";
-} else if (realTekAudio == true) {
+} else if (realTekAudio === true) {
   Audio = "BayTrailChipsetDriver-Lenovo-x86_64-pc-windows-msvc.exe";
 }
 const i2sInstaller = "BayTrailChipsetDriver-Lenovo-x86_64-pc-windows-msvc.exe";
@@ -162,19 +163,19 @@ let process;
 function installStatus() {
   if (processExists == null) {
     process = processName[0];
-  } else if (processExists == false && processStatus == 0) {
+  } else if (processExists === false && processStatus === 0) {
     process = processName[index];
     installDrivers(process);
-  } else if (processExists == true && processStatus == 0) {
+  } else if (processExists === true && processStatus === 0) {
     process = processName[index];
     //console.log(process);
     processStatus++;
-  } else if (processExists == true && processStatus == 1) {
-    if (processStatus < 2 || processStatus == 1) {
+  } else if (processExists === true && processStatus === 1) {
+    if (processStatus < 2 || processStatus === 1) {
       processStatus++;
     }
     console.log("running");
-  } else if (processExists == false && processStatus == 2) {
+  } else if (processExists === false && processStatus === 2) {
     index++;
     processStatus = 0;
     //console.log("process-closed");
@@ -185,51 +186,68 @@ function installStatus() {
 function startInstall() {
   setInterval(async () => {
     installStatus();
+    if(process !== undefined)
+    {
     processExists = await invoke("check_process", { value1: process });
+    }
   }, 1000);
+  document.getElementById('mainPage').style.display = "none"
+  document.getElementById('progressPage').style.display = "block"
+}
+
+function installFinished(){
+  document.getElementById('progressPage').style.display = "none"
+  document.getElementById('finished').style.display = "block"
+
 }
 
 //executes sidecars in order
 async function installDrivers(driver) {
   console.log(driver);
-  if (driver == vcDist) {
+  if (driver === vcDist) {
     crosVcDist.execute();
-  } else if (driver == ECFile) {
-    if (driver == "wilcoec.1.0.1-installer-x86_64-pc-windows-msvc.exe") {
+  } else if (driver === ECFile) {
+    if (driver === "wilcoec.1.0.1-installer-x86_64-pc-windows-msvc.exe") {
       crosWilcoEc.execute();
     } else {
       crosEc.execute();
     }
-  } else if (driver == touchPadInstaller) {
+  } else if (driver === touchPadInstaller) {
     if (
-      driver ==
+      driver ===
       "crostouchpad.4.1.4-onlyeve-installer-x86_64-pc-windows-msvc.exe"
     ) {
       crosTouchPadEve.execute();
     } else {
       crosTouchPad.execute();
     }
-  } else if (touchscreen == true && driver == touchScreenInstaller) {
+  } else if (touchscreen === true && driver === touchScreenInstaller) {
     crosTouchScreen.execute();
-  } else if (touchscreen == false && driver == touchScreenInstaller) {
+  } else if (touchscreen === false && driver === touchScreenInstaller) {
     index++;
-  } else if (i2sDriver == true && driver == i2sInstaller) {
+  } else if (i2sDriver === true && driver === i2sInstaller) {
     i2s.execute();
-  } else if (i2sDriver == false && driver == i2sInstaller) {
+  } else if (i2sDriver === false && driver === i2sInstaller) {
     index++;
-  } else if (max989090 == true && driver == Audio) {
-    if (boardname == "Cyan") {
+  } else if (max989090 === true && driver === Audio) {
+    if (boardname === "Cyan") {
       maxAudioR11.execute();
     } else {
       maxAudio.execute();
     }
-  } else if (samusAudio == true && driver == Audio) {
+  } else if (samusAudio === true && driver === Audio) {
     audioSamus.execute();
-  } else if (realTekAudio == true && driver == Audio) {
+  } else if (realTekAudio === true && driver === Audio) {
     realTek.execute();
+  }
+  else if(driver === undefined) {
+    installFinished();
   }
 }
 
-document.getElementById("install").addEventListener('keydown',startInstall());
-document.getElementById("install").addEventListener('mousedown',startInstall());
+//document.getElementById("install").addEventListener('keydown',startInstall());
+document.getElementById("install").addEventListener('mousedown',() => {startInstall()});
+document.getElementById("close").addEventListener('mousedown',() => {appWindow.close()});
+document.getElementById("close1").addEventListener('mousedown',() => {appWindow.close()});
+
 
