a63c0c3c3447f4dd3e779c2d9bb871e73214cb68
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2023-10-25 18:08:46 +0530

    Changed CSS to look good 	modified:   index.html 	modified:   src-tauri/build.rs 	modified:   src-tauri/tauri.conf.json 	modified:   src/styles.css

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    
+    /* 
   let mut windows = tauri_build::WindowsAttributes::new();
   windows = windows.app_manifest(r#"
   <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
@@ -14,7 +14,6 @@ fn main() {
   "#);
   let attrs =  tauri_build::Attributes::new().windows_attributes(windows);
   tauri_build::try_build(attrs).expect("failed to run build script");
-   /* 
+   */
     tauri_build::build()
-    */
 }
